1fb37c171b95322d137e4e0bc1a8dda3b6837826
Amir-Aminian <amir_aminian@hotmail.com>
2022-09-15 19:06:55 -0230

    added refe to singin component

--- src/components/SingIn.js
+++ src/components/SingIn.js
@@ -1,13 +1,24 @@
 import InputForm from "./forms/InputForm";
-
+import { useRef } from "react";
 
 const SingIn = () => {
+    const usernameRef = useRef();
+
+    const passwordRef = useRef();
+
+    const submit = () => {
+        console.log([
+            usernameRef.current.value,
+            passwordRef.current.value
+        ]);
+    }
+
     return (
-        <form className="singIn">
+        <form className="singIn" >
             <h2>Sing In</h2>
-            <InputForm type="text" nameId="username" text="Username:" />
-            <InputForm type="text" nameId="password" text="Password:" />
-            <input type="button" value={"Sing In"} />
+            <InputForm type="text" ref={usernameRef} nameId="username" text="Username:" />
+            <InputForm type="text" ref={passwordRef} nameId="password" text="Password:" />
+            <input type="button" onClick={submit} value={"Sing In"} />
         </form>
     );
 }
