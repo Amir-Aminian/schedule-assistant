9d777f35ba85fa3db2d7019cc0d8e905461d6de3
Amir-Aminian <amir_aminian@hotmail.com>
2022-09-21 22:36:11 -0230

    styled inputform component

--- src/components/forms/InputForm.js
+++ src/components/forms/InputForm.js
@@ -1,11 +1,11 @@
 import { forwardRef } from "react";
 import { InputLabel, TextField } from '@mui/material';
 
-const InputForm = ({ type, nameId, text, props }, ref) => {
+const InputForm = ({ type, nameId, text }, ref) => {
     return (
         <div>
             <InputLabel htmlFor={nameId}>{text}</InputLabel>
-            <TextField type={type} ref={ref} name={nameId} id={nameId} props />
+            <TextField type={type} ref={ref} name={nameId} id={nameId} variant="filled" />
         </div>
     )
 }
