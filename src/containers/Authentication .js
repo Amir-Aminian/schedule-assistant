ae07d9e13c5dc024f4dd9ff6f45187c1204ac93f
Amir-Aminian <amir_aminian@hotmail.com>
2022-10-05 21:51:30 -0230

    added style to authentication container

--- src/containers/Authentication .js
+++ src/containers/Authentication .js
@@ -1,21 +1,23 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import NavigationBar from "../components/authentication/NavigationBar";
 import SingIn from "../components/authentication/SingIn";
 import SingUp from "../components/authentication/SingUp";
 import ForgotPassword from "../components/authentication/ForgotPassword";
+import { Container } from "@mui/material";
 
 
 
 const Authentication = () => {
   return (
-    <BrowserRouter>
-      <Routes>        
-        <Route path="/" element={<SingIn />} />
-        <Route path="singUp" element={<SingUp />} />
-        <Route path="forgotPassword" element={<ForgotPassword />} />
-        <Route path="*" element={<h1>ERROR 404</h1>} />
-      </Routes>
-    </BrowserRouter>
+    <Container maxWidth="xs">
+      <BrowserRouter>
+        <Routes>        
+          <Route path="/" element={<SingIn />} />
+          <Route path="singUp" element={<SingUp />} />
+          <Route path="forgotPassword" element={<ForgotPassword />} />
+          <Route path="*" element={<h1>ERROR 404</h1>} />
+        </Routes>
+      </BrowserRouter>
+    </Container>
   );
 }
 
