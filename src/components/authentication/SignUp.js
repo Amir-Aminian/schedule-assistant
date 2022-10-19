24b0d0f858b8ecf74b8ed6641e9ca6bacea1ac19
Amir-Aminian <amir_aminian@hotmail.com>
2022-10-19 21:15:09 -0230

    added another rule to sign in component

--- src/components/authentication/SignUp.js
+++ src/components/authentication/SignUp.js
@@ -7,6 +7,8 @@ import { Register } from "../../utilities/Authentication";
 import { useForm } from "react-hook-form";
 
 const SignUp = () => {
+    const securityQuestions = ["In what city were you born?", "What is the name of your favorite pet?", "What is your mother's maiden name?", "What high school did you attend?", "What was the name of your elementary school?", "What was the make of your first car?", "What was your favorite food as a child?", "Where did you meet your spouse?", "What year was your father (or mother) born?"];
+
     const { control, handleSubmit, watch } = useForm();
 
     const navigate = useNavigate();
@@ -17,7 +19,10 @@ const SignUp = () => {
         navigate("/");
     };
 
-    const securityQuestions = ["In what city were you born?", "What is the name of your favorite pet?", "What is your mother's maiden name?", "What high school did you attend?", "What was the name of your elementary school?", "What was the make of your first car?", "What was your favorite food as a child?", "Where did you meet your spouse?", "What year was your father (or mother) born?"];
+    const handleOptions = (value1, value2) => {
+        let options = securityQuestions.filter((b) => b != value1 & b != value2);
+        return options;
+    };
 
     return (
         <Box>
@@ -43,11 +48,11 @@ const SignUp = () => {
                                 <p>Select and answer three security questions. These questions will help us verify your identity should you forget your password.</p>
                             </Grid>
                             <Grid container item direction="column" alignItems="center" justifyContent="center" spacing={2} width={400}>
-                                <DropDownForm id="SQ1" label={"Select first security question"} options={securityQuestions} control={control} rules={{required: "This field is required"}} />
+                                <DropDownForm id="SQ1" label={"Select first security question"} options={handleOptions(watch("SQ2"),watch("SQ3"))} control={control} rules={{required: "This field is required"}} />
                                 <InputForm type="text" id="SA1" label="Answer first security question" control={control} rules={{required: "This field is required"}} />
-                                <DropDownForm id="SQ2" label={"Select second security question"} options={securityQuestions} control={control} rules={{required: "This field is required"}} />
+                                <DropDownForm id="SQ2" label={"Select second security question"} options={handleOptions(watch("SQ1"),watch("SQ3"))} control={control} rules={{required: "This field is required"}} />
                                 <InputForm type="text" id="SA2" label="Answer second security question" control={control} rules={{required: "This field is required"}} />
-                                <DropDownForm id="SQ3" label={"Select third security question"} options={securityQuestions} control={control} rules={{required: "This field is required"}} />
+                                <DropDownForm id="SQ3" label={"Select third security question"} options={handleOptions(watch("SQ1"),watch("SQ2"))} control={control} rules={{required: "This field is required"}} />
                                 <InputForm type="text" id="SA3" label="Answer third security question" control={control} rules={{required: "This field is required"}} />
                                 <Grid container item justifyContent="flex-end">
                                     <Link to={"/"}>Already have an account? Sign In</Link>
