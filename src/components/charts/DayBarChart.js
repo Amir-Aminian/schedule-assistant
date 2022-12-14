a8b3c497d11b05a33726b226ffeb98eb6b34a48e
Amir-Aminian <amir_aminian@hotmail.com>
2022-12-13 20:50:28 -0330

    added box to the day bar chart component

--- src/components/charts/DayBarChart.js
+++ src/components/charts/DayBarChart.js
@@ -2,6 +2,7 @@ import { Bar } from "react-chartjs-2";
 import {chart as chartjs} from "chart.js/auto"
 import {Chart} from 'chart.js';
 import 'chartjs-adapter-luxon';
+import { Box } from "@mui/material";
 
 const DayBarChart = ({dayTasks}) => {
     const clickHandler = (e, element) => {
@@ -11,44 +12,48 @@ const DayBarChart = ({dayTasks}) => {
     };
 
     return (
-        <Bar
-            data={{
-                datasets:
-                    dayTasks.map((data) =>( 
-                        {data:[{x:[data.task.startTime, data.task.endTime],y:data.user}], backgroundColor:data.color}
-                    ))
-            }} 
-            options={{
-                onClick:(e, element) => clickHandler(e, element),
-                maintainAspectRatio:false,
-                indexAxis:"y",
-                borderSkipped:false,
-                borderRadius:"5", 
-                barPercentage:"0.2", 
-                plugins:{
-                    legend:{display:false},
-                    tooltip:{enabled:false}                    
-                }, 
-                scales:{
-                    x:{
-                        type:"time",
-                        min:"00:00",
-                        max:"24:00",
-                        time:{
-                            unit:"hour",
-                            displayFormats:{hour:"ha"}
-                       }
-                    }, 
-                    y:{
-                        stacked:true,
-                        grid:{display:false},
-                        ticks:{
-                            font:{size:"15%"}
+        <Box>
+            <Box>
+                <Bar
+                    data={{
+                        datasets:
+                            dayTasks.map((data) =>( 
+                                {data:[{x:[data.task.startTime, data.task.endTime],y:data.user}], backgroundColor:data.color}
+                            ))
+                    }} 
+                    options={{
+                        onClick:(e, element) => clickHandler(e, element),
+                        maintainAspectRatio:false,
+                        indexAxis:"y",
+                        borderSkipped:false,
+                        borderRadius:"5", 
+                        barPercentage:"0.2", 
+                        plugins:{
+                            legend:{display:false},
+                            tooltip:{enabled:false}                    
+                        }, 
+                        scales:{
+                            x:{
+                                type:"time",
+                                min:"00:00",
+                                max:"24:00",
+                                time:{
+                                    unit:"hour",
+                                    displayFormats:{hour:"ha"}
+                            }
+                            }, 
+                            y:{
+                                stacked:true,
+                                grid:{display:false},
+                                ticks:{
+                                    font:{size:"15%"}
+                                }
+                            }
                         }
-                    }
-                }
-            }}
-         />
+                    }}
+                />
+            </Box>
+        </Box>
     );
 }
 
